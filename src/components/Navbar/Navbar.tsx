43d291c2625a99951d7c52d2aa3a24a382f59d76
Deepdarshan <deepdarshan21@gmail.com>
2022-03-10 19:19:59 +0530

    Add navbar

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,13 @@
+import { IonTitle, IonToolbar } from "@ionic/react";
+
+const Navbar: React.FC = () => {
+    return (
+        <>
+            <IonToolbar>
+                <IonTitle>Assignment</IonTitle>
+            </IonToolbar>
+        </>
+    );
+};
+
+export default Navbar;
